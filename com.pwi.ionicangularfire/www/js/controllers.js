10096cd6a6c8d2398d7f3aa6dbb7d6c81d6dda66
Melanie Liang <melly.strearshy@gmail.com>
2016-07-01 11:01:32 -0600

    create list

--- com.pwi.ionicangularfire/www/js/controllers.js
+++ com.pwi.ionicangularfire/www/js/controllers.js
@@ -2,7 +2,7 @@ angular.module('starter.controllers', [])
 
 .controller('DashCtrl', function($scope) {})
 
-.controller('ChatsCtrl', function($scope, Chats) {
+.controller('ListsCtrl', function($scope, Lists) {
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
   // To listen for when this page is active (for example, to refresh data),
@@ -11,14 +11,14 @@ angular.module('starter.controllers', [])
   //$scope.$on('$ionicView.enter', function(e) {
   //});
 
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
+  $scope.lists = Lists.all();
+  $scope.remove = function(list) {
+    Lists.remove(list);
   };
 })
 
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
+.controller('ListDetailCtrl', function($scope, $stateParams, Lists) {
+  $scope.list = Lists.get($stateParams.listId);
 })
 
 .controller('AccountCtrl', function($scope) {
--- com.pwi.ionicangularfire/www/js/services.js
+++ com.pwi.ionicangularfire/www/js/services.js
@@ -1,47 +1,40 @@
 angular.module('starter.services', [])
 
-.factory('Chats', function() {
+.factory('Lists', function() {
   // Might use a resource here that returns a JSON array
 
   // Some fake testing data
-  var chats = [{
+  var lists = [{
     id: 0,
-    name: 'Ben Sparrow',
-    lastText: 'You on your way?',
-    face: 'img/ben.png'
+    name: 'Doom 4',
+    price: '$49.99',
+    description: 'The game is played entirely from a first-person perspective, with players taking the role of an unnamed marine, as he battles demonic forces from Hell that have been unleashed by the Union Aerospace Corporation on a future-set colonized planet Mars.',
+    face: 'img/Doom.jpg'
   }, {
     id: 1,
-    name: 'Max Lynx',
-    lastText: 'Hey, it\'s me',
-    face: 'img/max.png'
+    name: 'Mario 3D World',
+    price: '$60',
+    description: 'The game follows a similar play style to Super Mario 3D Land, which combines the free-roaming gameplay of 3D Mario titles with the mechanics of 2D side-scrolling platforming games in the series, going so far as to include a flagpole and timer on every level.',
+    face: 'img/Mario.jpg'
   }, {
     id: 2,
-    name: 'Adam Bradleyson',
-    lastText: 'I should buy a boat',
-    face: 'img/adam.jpg'
-  }, {
-    id: 3,
-    name: 'Perry Governor',
-    lastText: 'Look at my mukluks!',
-    face: 'img/perry.png'
-  }, {
-    id: 4,
-    name: 'Mike Harrington',
-    lastText: 'This is wicked good ice cream.',
-    face: 'img/mike.png'
+    name: ' Super Metroid',
+    price: '$46.99',
+    description: 'Super Metroid is a direct sequel to Metroid II: Return of Samus (1991). The player controls bounty hunter Samus Aran as she attempts to retrieve a Metroid larva stolen by the Space Pirate leader Ridley. The gameplay focuses on exploration, with the player searching for power-ups that are used to reach previously inaccessible areas. It introduces new concepts to the series, such as the inventory screen, an automapping facility, and the ability to fire in all directions.',
+    face: 'img/metroid.jpg'
   }];
 
   return {
     all: function() {
-      return chats;
+      return lists;
     },
-    remove: function(chat) {
-      chats.splice(chats.indexOf(chat), 1);
+    remove: function(list) {
+      lists.splice(lists.indexOf(list), 1);
     },
-    get: function(chatId) {
-      for (var i = 0; i < chats.length; i++) {
-        if (chats[i].id === parseInt(chatId)) {
-          return chats[i];
+    get: function(listId) {
+      for (var i = 0; i < lists.length; i++) {
+        if (lists[i].id === parseInt(listId)) {
+          return lists[i];
         }
       }
       return null;
